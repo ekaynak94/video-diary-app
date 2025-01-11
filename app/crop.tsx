76615fd73dc572f948725ca7ed12bc7911406879
Emre Kaynak <emre@MacBook-Pro.local>
2025-01-11 17:46:58 +0300

    initial crop modal ui

--- app/crop.tsx
+++ app/crop.tsx
@@ -1,18 +1,63 @@
-import React, { useEffect } from "react";
-import { View, Text } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import React, { useState } from "react";
+import { View, TouchableOpacity } from "react-native";
+import { VideoView, useVideoPlayer } from "expo-video";
+import { Ionicons } from "@expo/vector-icons";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import CustomButton from "../components/CustomButton";
 
-export default function Crop() {
+export default function CropModal() {
   const params = useLocalSearchParams();
+  const router = useRouter();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const player = useVideoPlayer(params.videoUri);
 
-  useEffect(() => {
-    console.log("Navigated to Crop with videoUri:", params.videoUri);
-  }, [params]);
+  const handlePlayPause = () => {
+    if (isPlaying) {
+      player.pause();
+    } else {
+      player.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleFullscreen = () => {};
 
   return (
-    <View className="flex-1 py-safe dark:bg-grey">
-      <Text className="text-white">Crop Modal</Text>
-      <Text className="text-white">Video URI: {params.videoUri}</Text>
+    <View className="flex-1 pb-safe dark:bg-grey">
+      <View className="relative w-full flex-1">
+        <VideoView
+          player={player}
+          style={{ height: "100%", width: "auto", maxWidth: "100%" }}
+          nativeControls={false}
+        />
+        <TouchableOpacity
+          className="absolute top-4 left-4 bg-black/30 p-2 rounded-full"
+          onPress={() => router.back()}
+        >
+          <Ionicons name="arrow-back" size={24} color="white" />
+        </TouchableOpacity>
+        <View className="absolute bottom-4 left-4 right-4 flex-row justify-between">
+          <TouchableOpacity
+            className="p-4 bg-black/30 rounded-full"
+            onPress={handlePlayPause}
+          >
+            <Ionicons
+              name={isPlaying ? "pause" : "play"}
+              size={24}
+              color="white"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            className="p-4 bg-black/30 rounded-full"
+            onPress={handleFullscreen}
+          >
+            <Ionicons name="expand" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View className="p-4">
+        <CustomButton onPress={() => {}} title="Crop" />
+      </View>
     </View>
   );
 }
